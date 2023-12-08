cdc9ad4ae8f080b555c88e336f51615de993df77
portatil arch <rogerfiblasalvado@gmail.com>
2023-12-08 22:58:26 +0100

    fix error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::{
     io::{stdin, Write},
     path::PathBuf,
@@ -11,12 +12,11 @@ use terminal_menu::{menu, button, run, mut_menu, label};
 fn is_img(file_path: &PathBuf) -> bool {
     let img_files: Vec<&str> = vec!["jpeg", "jpg", "png", "gif", "pnm", "tga", "ttf", "webp", "bmp", "farb", "farbfeld"];
 
-    let a = &file_path.extension();
-
-    if img_files.contains(&a.unwrap().to_str().unwrap()) {
-        return true; 
+    if let Some(extension) = file_path.extension() {
+        if let Some(ext_str) = extension.to_str() {
+            return img_files.contains(&ext_str);
+        }
     }
-
     false
 }
 
@@ -25,8 +25,9 @@ fn clear_screen() {
 }
 
 fn main() {
-    let conf_path: PathBuf = home_dir().expect("Error getting home dir")
-                                .join(".config/bgc/config.conf");
+    let conf_path: PathBuf = home_dir()
+        .expect("Error getting home dir")
+        .join(".config/bgc/config.conf");
 
     if !conf_path.exists() {
         let mut w_input: String = String::new();
@@ -34,70 +35,78 @@ fn main() {
         println!("Enter the absolute path of the wallpapers");
 
         stdin().read_line(&mut w_input)
-            .expect("Error reading path.");
+            .expect("Error getting input.");
 
         let w_path: PathBuf = PathBuf::from(w_input.trim());
         
         if !w_path.exists() || !w_path.is_dir() {
-             println!("The path doesnt exist or its not a directory");
-             exit(1);
+             panic!("The path doesnt exist or its not a directory");
         }
 
-        fs::DirBuilder::new()
+        match fs::DirBuilder::new()
             .recursive(true)
-            .create(home_dir().expect("Error").join(".config/bgc/"))
-            .unwrap();
+            .create(home_dir().expect("Error getting home directory").join(".config/bgc/")) {
+                Ok(()) => (),
+                Err(error) => panic!("Error creating directory: {}", error),
+            };
 
-        let mut c_file = File::create(&conf_path)
-            .expect("Error creating file");
 
-        let text: String = String::from("wallpaper_path = ") + w_path.to_str().unwrap();
+        let mut c_file: File = match File::create(&conf_path) {
+            Ok(file) => file,
+            Err(err) => panic!("Error creating config file\n{}", err),
+        };
 
-        c_file.write_all(text.as_bytes())
-            .expect("Error writing to file");
+        let text: String = String::from("wallpaper_path = ") + w_path.to_str()
+                                                                .expect("Error converting to str");
 
-        c_file.flush()
-            .expect("Error flushing file");
+        match c_file.write_all(text.as_bytes()) {
+            Ok(()) => (),
+            Err(err) => panic!("Error writing to file\n{}", err),
+        };
+
+        match c_file.flush() {
+            Ok(()) => (),
+            Err(err) => panic!("Error flushing file\n{}", err),
+        };
 
         clear_screen();
     } 
 
     let config_file_contents: String = fs::read_to_string(conf_path)
-                                .expect("Error reading file");
+                                .unwrap();
     
     let mut wallpapers_path: PathBuf = PathBuf::new();
 
     for line in config_file_contents.lines() {
-        // GET WALLPAPER PATH
         if line.trim().starts_with("wallpaper_path") {
             let wallpapers_path_s: &str = line
                 .split('=')
                 .nth(1)
-                .map(|s| s.trim())
-                .unwrap_or_default();
-
-            wallpapers_path = PathBuf::from(wallpapers_path_s);
+                .expect("No wallpaper_path on config file");
+            wallpapers_path = PathBuf::from(wallpapers_path_s.trim());
         }
     }
 
     if !wallpapers_path.is_dir() {
         println!("The path stored in the config file is not a path");
         println!("Config file: ~/.config/bgc/config.conf");
-        println!("Wallpapers directory provided: {}", wallpapers_path.to_str().unwrap());
+        println!("Wallpapers directory provided: {}", wallpapers_path.to_str().expect("No file provided"));
         exit(1);
     }
 
     let mut images: Vec<String> = Vec::new();
     let mut images_p: Vec<PathBuf> = Vec::new();
 
-    for path in fs::read_dir(&wallpapers_path).unwrap() {
-        if let Ok(entry) = path  {
+    for path in fs::read_dir(&wallpapers_path).expect("Couldn't read directory") {
+        if let Ok(entry) = path {
             let path_buf = entry.path();
             if is_img(&path_buf) {
-                let str_path: String = String::from(path_buf.to_str().unwrap())
+                let str_path: String = String::from(path_buf.to_str()
+                                                    .expect("Error converting to string")
+                                                )
                     .split('/')
                     .last()
-                    .unwrap()
+                    .expect("Error geting &str")
                     .to_string();
                 images.push(str_path);
                 images_p.push(path_buf);
@@ -107,7 +116,8 @@ fn main() {
     
     let vstring: String = "v".to_string() + env!("CARGO_PKG_VERSION") + "\n";
 
-    let buttons: Vec<_> = images.iter()
+    let buttons: Vec<_> = images
+                    .iter()
                     .map(|image| button(image))
                     .collect();
 
@@ -132,7 +142,6 @@ fn main() {
     menu_v.push(button("Quit"));
 
     let menu = menu(menu_v);
-
     run(&menu);
 
     let menu_result = mut_menu(&menu);
@@ -157,19 +166,20 @@ fn main() {
     let image_path: String = match selected_path {
         Some(path) => path.to_string_lossy().to_string(),
         None => {
-            println!("Selected path not found");
-            exit(1);
+            panic!("Selected path not found");
         }
     };
 
-    Command::new("swww")
-        .arg("img")
-        .arg(&image_path)
-        .arg("--transition-step")
-        .arg("30")
-        .arg("--transition-fps")
-        .arg("60")
-        .spawn()
-        .expect("swww is not installed");
+    match Command::new("swww")
+            .arg("img")
+            .arg(&image_path)
+            .arg("--transition-step")
+            .arg("30")
+            .arg("--transition-fps")
+            .arg("60")
+            .spawn() {
+                Ok(child) => child,
+                Err(err) => panic!("Swww not installed\n{}", err),
+            };
 
 }
